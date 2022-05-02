5aa2d853daa95a42473b4098c3ea355820a7b324
Rushabh Pandav <94757591+Rushpandav96@users.noreply.github.com>
2022-05-02 17:19:10 +0530

    g

---
+++ src/Redux/authaction.js
@@ -0,0 +1,18 @@
+export const LOGIN  = "LOGIN";
+export const LOGOUT = 'LOGUT';
+
+
+const login = (user) => {
+    return {
+        type:LOGIN,
+        payload:user
+    }
+}
+
+const logout = () => {
+    return {
+        type:LOGOUT
+    }
+}
+
+export {login,logout};
